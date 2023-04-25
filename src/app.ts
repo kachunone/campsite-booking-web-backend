0150d31a1680e802a8ebe38d0f393a20891af467
morris <kajimone@gmail.com>
2023-04-25 02:21:36 -0400

    setted up dev env

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Hello, Typscript!");
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
